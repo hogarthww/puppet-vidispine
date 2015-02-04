49075cfecf89f032c3cecd4b4788935cc1b57240
Richard Davies <richard.davies@hogarthww.com>
2015-02-04 00:38:27 +0000

    improve imq broker cluster config

--- manifests/install.pp
+++ manifests/install.pp
@@ -302,40 +302,92 @@ class vidispine::install {
     subscribe   => File["${vidispine::installer_dir}/Vidispine_${vidispine::vidispine_version}/config.xml"],
   }
 
+  # we need to have started imqbrokerd before configuring the enhanced broker cluster
   if ($vidispine::glassfish_imq_cluster_enable) {
+
+    $imq_conf = "${vidispine::glassfish_parent_dir}/${vidispine::glassfish_install_dir}/glassfish/domains/${vidispine::glassfish_domain_name}/imq/instances/imqbroker/props/config.properties"
+
+    $glassfish_imq_brokers = join(sort(keys($vidispine::glassfish_imq_broker_list)), ',')
 
     ini_setting { 'imq-brokerid' :
       ensure            => present,
       key_val_separator => '=',
-      section           => '',
-      path              => "${vidispine::glassfish_parent_dir}/${vidispine::glassfish_install_dir}/glassfish/domains/${vidispine::glassfish_domain_name}/imq/instances/imqbroker/props/config.properties",
+      path              => $imq_conf,
       setting           => 'imq.brokerid',
       value             => $::fqdn,
       require           => Exec['vidispine-installer'],
     }
 
-    ini_setting { 'imq-cluster-url' :
+    ini_setting { 'imq-cluster-ha' :
       ensure            => present,
       key_val_separator => '=',
-      section           => '',
-      path              => "${vidispine::glassfish_parent_dir}/${vidispine::glassfish_install_dir}/glassfish/domains/${vidispine::glassfish_domain_name}/imq/instances/imqbroker/props/config.properties",
-      setting           => 'imq.cluster.url',
-      value             => "${vidispine::glassfish_parent_dir}/${vidispine::glassfish_install_dir}/glassfish/domains/${vidispine::glassfish_domain_name}/imq/instances/imqbroker/props/cluster.properties",
-      require           => Exec['vidispine-installer'],
+      path              => $imq_conf,
+      setting           => 'imq.cluster.ha',
+      value             => 'true',
+      require           => Ini_setting['imq-brokerid'],
     }
 
-    $glassfish_imq_brokers = join(sort(keys($vidispine::glassfish_imq_broker_list)), ',')
+    ini_setting { 'imq-cluster-clusterid' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.cluster.clusterid',
+      value             => $vidispine::glassfish_domain_name,
+      require           => Ini_setting['imq-cluster-ha'],
+    }
 
-    file {"${vidispine::glassfish_parent_dir}/${vidispine::glassfish_install_dir}/glassfish/domains/${vidispine::glassfish_domain_name}/imq/instances/imqbroker/props/cluster.properties":
-      ensure  => present,
-      owner   => $vidispine::glassfish_user,
-      group   => $vidispine::glassfish_group,
-      mode    => '0644',
-      content => template("vidispine/cluster.properties.erb"),
-      require => [
-                   Ini_setting['imq-brokerid'],
-                   Ini_setting['imq-cluster-url'],
-                 ]
+    ini_setting { 'imq-cluster-brokerlist' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.cluster.brokerlist',
+      value             => $glassfish_imq_brokers,
+      require           => Ini_setting['imq-cluster-clusterid'],
+    }
+
+    ini_setting { 'imq-persist-store' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.persist.store',
+      value             => 'jdbc',
+      require           => Ini_setting['imq-cluster-brokerlist'],
+    }
+
+    ini_setting { 'imq-persist-jdbc-dbvendor' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.persist.jdbc.dbVendor',
+      value             => 'postgresql',
+      require           => Ini_setting['imq-persist-store'],
+    }
+
+    ini_setting { 'imq-persist-jdbc-postgresql-opendburl' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.persist.jdbc.postgresql.opendburl',
+      value             => "jdbc\\:postgresql\\://${vidispine::postgresql_host}\\:${vidispine::postgresql_port}/${vidispine::postgresql_imq_database}",
+      require           => Ini_setting['imq-persist-jdbc-dbvendor'],
+    }
+
+    ini_setting { 'imq-persist-jdbc-postgresql-user' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.persist.jdbc.postgresql.user',
+      value             => $vidispine::postgresql_imq_user,
+      require           => Ini_setting['imq-persist-jdbc-postgresql-opendburl'],
+    }
+
+    ini_setting { 'imq-persist-jdbc-postgresql-password' :
+      ensure            => present,
+      key_val_separator => '=',
+      path              => $imq_conf,
+      setting           => 'imq.persist.jdbc.postgresql.password',
+      value             => $vidispine::postgresql_imq_password,
+      require           => Ini_setting['imq-persist-jdbc-postgresql-user'],
     }
 
   }
