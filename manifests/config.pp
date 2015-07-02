72e934c2230c3a4e187f8522048d8d299ec040f0
Thomas O'Donnell <thomas.odonnell@hogarthww.com>
2015-07-02 10:06:52 +0100

    Whitespace corection.

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,32 +14,32 @@ class vidispine::config {
 
     # set server http thread pool size if we are running standalone vidispine
     set { 'server-config.thread-pools.thread-pool.http-thread-pool.max-thread-pool-size':
-      value        => $vidispine::vidispine_http_pool_size,
+      value => $vidispine::vidispine_http_pool_size,
     }
 
     # set server http thread pool timeout if we are running standalone vidispine
     set { 'server-config.thread-pools.thread-pool.http-thread-pool.idle-thread-timeout-seconds':
-      value        => $vidispine::vidispine_http_pool_timeout,
+      value => $vidispine::vidispine_http_pool_timeout,
     }
 
     # set server noauth thread pool size if we are running standalone vidispine
     set { 'server-config.thread-pools.thread-pool.noauth-pool.max-thread-pool-size':
-      value        => $vidispine::vidispine_noauth_pool_size,
+      value => $vidispine::vidispine_noauth_pool_size,
     }
 
     # set server noauth thread pool idle timeout if we are running standalone vidispine
     set { 'server-config.thread-pools.thread-pool.noauth-pool.idle-thread-timeout-seconds':
-      value        => $vidispine::vidispine_noauth_pool_timeout,
+      value => $vidispine::vidispine_noauth_pool_timeout,
     }
 
     # set server solr thread pool size if we are running standalone vidispine
     set { 'server-config.thread-pools.thread-pool.solr-pool.max-thread-pool-size':
-      value        => $vidispine::vidispine_solr_pool_size,
+      value => $vidispine::vidispine_solr_pool_size,
     }
 
     # set vidispine jdbc pool max size if we running the glassfish das
     set { 'resources.jdbc-connection-pool.VidispinePool.max-pool-size':
-      value        => $vidispine::vidispine_jdbc_max_size,
+      value => $vidispine::vidispine_jdbc_max_size,
     }
   }
 
