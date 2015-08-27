9688a405fd610c0e66902c901d79b5347c6fa343
Doug Neal <doug@neal.me.uk>
2015-08-27 17:31:28 +0100

    [TP-478] providers: port vidispine_storage_method to REST client
    Some test failures fixed

--- lib/puppet/provider/vidispine_storage_method/vidispine_storage_method.rb
+++ lib/puppet/provider/vidispine_storage_method/vidispine_storage_method.rb
@@ -4,11 +4,77 @@ require Pathname.new(__FILE__).dirname.dirname.expand_path + 'vidispine'
 Puppet::Type.type(:vidispine_storage_method).provide(:vidispine_storage_method, :parent => Puppet::Provider::Vidispine) do
   desc "Vidispine Storage Method"
 
+  # The following method re-implements the exists? method of the vidispine_storage provider.
+  #
+  # I think that if vidispine_storage implements self.prefetch, this won't be needed,
+  # as all the storage resources will already have been pre-instantiated.
+  #
+  def find_parent
+    # This is the only difference: search for the storage using the storage_method's
+    # location attribute.
+    name = @resource[:location]
+
+    begin
+      response = self.rest_get '/API/storage/'
+
+      if response['storage'].nil? then
+        return false
+      else
+        response['storage'].each do |storage|
+          metadataFields = storage["metadata"]["field"]
+          metadataFields.each do |metadataField|
+            if metadataField['key'] == 'storageName' && metadataField['value'] == name then
+              return storage['id']
+            end
+          end
+        end
+
+        return false
+      end
+
+    rescue Exception
+      raise Puppet::Error, "Failed to query Vidispine for Storage #{name}: #{$!}"
+    end
+  end
+
+
+  def find_vxid
+    begin
+      parent = find_parent()
+
+      # Perhaps the location attribute points to a nonexistent storage? In that case
+      # parent will be set to false by the find_parent method, and we already know
+      # the storage_method doesn't exist as described.
+      #
+      if parent == false then
+        return false
+      end
+
+      response = self.rest_get "/API/storage/#{parent}/method"
+
+      unless response['method'].nil? then
+        response['method'].each do |method|
+          uri = method['uri']
+          if uri == @resource[:storageuri]
+          then
+            return method['id']
+          end
+        end
+      end
+
+      return false
+
+    rescue Exception
+      raise Puppet::Error, "Failed to query Vidispine for Storage Method #{name}: #{$!}"
+    end
+  end
+
+
   def create
     name = @resource[:name]
 
     begin
-      parent = findParent()
+      parent = find_parent()
       uripath = sprintf("/API/storage/%s/method?url=%s&read=%s&browse=%s&write=%s&type=%s",
         parent,
         @resource[:storageuri],
@@ -26,80 +92,26 @@ Puppet::Type.type(:vidispine_storage_method).provide(:vidispine_storage_method, 
   end
 
 
-  # The following method re-implements the exists? method of the vidispine_storage provider.
-  #
-  # I think that if vidispine_storage implements self.prefetch, this won't be needed,
-  # as all the storage resources will already have been pre-instantiated.
-  #
-  def findParent
-    # This is the only difference: search for the storage using the storage_method's
-    # location attribute.
-    name = @resource[:location]
-
-    begin
-      response = self.rest_get '/API/storage/'
-      storages = response["storage"]
-
-      storages.each do |storage|
-        metadataFields = storage["metadata"]["field"]
-        metadataFields.each do |metadataField|
-          if metadataField['key'] == 'storageName' && metadataField['value'] == name then
-            return storage['id']
-          end
-        end
-      end
-
+  def exists?
+    # An existence check is exactly the same as finding the VX-ID, but we always
+    # want to return a boolean value
+    #
+    if find_vxid then
+      return true
+    else
       return false
+    end
+  end
+
+
+  def destroy
+    begin
+      parent = find_parent()
+      id = find_vxid()
+      self.rest_delete "/API/storage/#{parent}/method/#{id}"
 
     rescue Exception
-      raise Puppet::Error, "Failed to query Vidispine for Storage #{name}: #{$!}"
+      raise Puppet::Error, "Failed to destroy Vidispine Storage Method #{name}: #{$!}"
     end
-  end
-
-
-  #there's a possible bug here to do with changing a storage metod and getting a different vxid.
-  #not sure what to do about this but at this prsent moment i dont care
-  def exists?
-    parent = findParent
-    vsurl  = "http://" + @resource[:vshostname] +":"+ @resource[:vsport] + "/API/storage/"+ parent + "/method"
-    name   = @resource[:name]
-    vsuser = @resource[:vsuser]
-    vspass = @resource[:vspass]
-    uri = URI.parse(vsurl)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth vsuser, vspass
-    request["Accept"] = "application/json"
-    response = http.request(request)
-    if response.body == 'null' || response.body == '{}' then return false end
-    parsed = JSON.parse(response.body)
-    methods = parsed["method"]
-    methods.each do |method|
-       uri  = method['uri']
-       if uri == @resource[:storageuri]
-       then
-#         return method['id']
-          return true
-       end
-    end
-   return false
-  end
-  
-  def destroy
-    parent = findParent()
-    id = exists?()
-    vsurl  = "http://" + @resource[:vshostname] +":"+ @resource[:vsport] + "/API/storage/"+ parent + "/method/" + id
-    name   = @resource[:name]
-    vsuser = @resource[:vsuser]
-    vspass = @resource[:vspass]
-
-    # Need to find storage method VX-ID
-
-    uri = URI.parse(vsurl)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Delete.new(uri.request_uri)
-    request.basic_auth vsuser, vspass
-    request["Accept"] = "application/json"
-    response = http.request(request)
   end
 end
