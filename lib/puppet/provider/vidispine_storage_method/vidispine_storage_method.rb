042ff47fbf077b2186f94b0f0905043ef726c878
Doug Neal <doug@neal.me.uk>
2015-08-27 16:12:15 +0100

    [TP-478] providers: rewrite vidispine_storage_method

--- lib/puppet/provider/vidispine_storage_method/vidispine_storage_method.rb
+++ lib/puppet/provider/vidispine_storage_method/vidispine_storage_method.rb
@@ -1,55 +1,59 @@
-require 'json'
-require 'net/http'
-require 'uri'
+require 'pathname'
+require Pathname.new(__FILE__).dirname.dirname.expand_path + 'vidispine'
 
-Puppet::Type.type(:vidispine_storage_method).provide(:vidispine_storage_method) do
-  desc "Vidispine Storage method provider."
-
-#this is a bit odd - because we dont actually use XML but a put to an existing storage
-#this means that all the work is done by the vsquery param (yay?)
+Puppet::Type.type(:vidispine_storage_method).provide(:vidispine_storage_method, :parent => Puppet::Provider::Vidispine) do
+  desc "Vidispine Storage Method"
 
   def create
-    parent = findParent()
-    name    = @resource[:name]
-    vsurl   = "http://" + @resource[:vshostname] +":"+ @resource[:vsport] + "/API/storage/" + parent + "/method"
-    vsquery = "url="+ @resource[:storageuri] +"&read="+ @resource[:read] +"&browse="+ @resource[:write] +"&write=" + @resource[:browse] +"&type=" + @resource[:type]
-    vsurl   = vsurl + "?" + vsquery
-    vsuser  = @resource[:vsuser]
-    vspass  = @resource[:vspass]
-    uri     = URI(vsurl)
-    http    = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Put.new(uri.request_uri)
-    request.basic_auth @resource[:vsuser], @resource[:vspass]
-    request["Content-Type"] = "application/xml"
-    response = http.request(request)
-  end
- 
-  #because we need to find the parent by a feild in the metadata (and these functions are difficult to share)
-  #we need to re-define the call that works out if the parent exists and returns its VX-ID
+    name = @resource[:name]
 
-  def findParent
-    vsurl  = "http://" + @resource[:vshostname] +":"+ @resource[:vsport] + "/API/storage/"
-    name   = @resource[:location]
-    vsuser = @resource[:vsuser]
-    vspass = @resource[:vspass]
-    uri = URI.parse(vsurl)
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    request.basic_auth vsuser, vspass
-    request["Accept"] = "application/json"
-    response = http.request(request)
-    if response.body == 'null' || response.body == '{}' then return false end
-    parsed = JSON.parse(response.body)
-    storages = parsed["storage"]
-    storages.each do |storage|
-       metadataFields = storage["metadata"]["field"]
-       metadataFields.each do |metadataField|
-         if metadataField['key'] == 'storageName' && metadataField['value'] == name then
-           return storage['id']
-         end
-       end
+    begin
+      parent = findParent()
+      uripath = sprintf("/API/storage/%s/method?url=%s&read=%s&browse=%s&write=%s&type=%s",
+        parent,
+        @resource[:storageuri],
+        @resource[:read].to_s,
+        @resource[:write].to_s,
+        @resource[:browse].to_s,
+        @resource[:type])
+
+      # We're PUTting a zero length document. All the work is done by the query string.
+      self.rest_put uripath, nil
+
+    rescue Exception
+      raise Puppet::Error, "Failed to create Vidispine Storage Method #{name}: #{$!}"
     end
-    return false
+  end
+
+
+  # The following method re-implements the exists? method of the vidispine_storage provider.
+  #
+  # I think that if vidispine_storage implements self.prefetch, this won't be needed,
+  # as all the storage resources will already have been pre-instantiated.
+  #
+  def findParent
+    # This is the only difference: search for the storage using the storage_method's
+    # location attribute.
+    name = @resource[:location]
+
+    begin
+      response = self.rest_get '/API/storage/'
+      storages = response["storage"]
+
+      storages.each do |storage|
+        metadataFields = storage["metadata"]["field"]
+        metadataFields.each do |metadataField|
+          if metadataField['key'] == 'storageName' && metadataField['value'] == name then
+            return storage['id']
+          end
+        end
+      end
+
+      return false
+
+    rescue Exception
+      raise Puppet::Error, "Failed to query Vidispine for Storage #{name}: #{$!}"
+    end
   end
 
 
