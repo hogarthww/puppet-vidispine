ff459538f01176fcbb3ab40e26edae9866697942
Doug Neal <doug@neal.me.uk>
2015-08-27 14:51:37 +0100

    [TP-478] Give the REST methods an options hash
    The options hash can be used to pass in extra instructions on how to handle
    the request, most notably requesting a JSON or XML response via the HTTP
    Accept header.

--- lib/puppet/provider/vidispine.rb
+++ lib/puppet/provider/vidispine.rb
@@ -4,30 +4,33 @@ class Puppet::Provider::Vidispine < Puppet::Provider
   require 'uri'
   require 'json'
 
-  def rest_get (endpoint)
-    return rest_action(endpoint, Net::HTTP::Get)
+  def rest_get (endpoint, options = {})
+    return rest_action(endpoint, Net::HTTP::Get, nil, options)
   end
 
-  def rest_post (endpoint, content)
-    return rest_action(endpoint, Net::HTTP::Post, content)
+  def rest_post (endpoint, content, options = {})
+    return rest_action(endpoint, Net::HTTP::Post, content, options)
   end
   
-  def rest_put (endpoint, content)
-    return rest_action(endpoint, Net::HTTP::Put, content)
+  def rest_put (endpoint, content, options = {})
+    return rest_action(endpoint, Net::HTTP::Put, content, options)
   end
 
-  def rest_delete (endpoint)
-    return rest_action(endpoint, Net::HTTP::Delete)
+  def rest_delete (endpoint, options = {})
+    return rest_action(endpoint, Net::HTTP::Delete, nil, options)
   end
   
-  def rest_action (endpoint, method, content=nil)
+  def rest_action (endpoint, method, content, options)
     uri  = URI("http://" + @resource[:vshostname] +":"+ @resource[:vsport] + endpoint)
     
     http    = Net::HTTP.new(uri.host, uri.port)
     request = method.new(uri.request_uri)
 
     request.basic_auth @resource[:vsuser], @resource[:vspass]
-    request['Accept'] = 'application/json'
+
+    if options[:accept] then
+      request['Accept'] = options[:accept]
+    end
 
     if not content.nil? then
       request.body = content
@@ -45,12 +48,19 @@ class Puppet::Provider::Vidispine < Puppet::Provider
       raise Puppet::Error, "Vidispine responded with HTTP #{response.code}: \"#{response.body}\""
     end
 
-    if response.body == 'null' or response.body == '{}' then
-      return {}
-    else
-      return JSON.parse(response.body)
+    # It seems that whenever the providers are interested in parsing the response
+    # they request JSON in the Accept header. For now I'm going to ignore the response
+    # body if it's not JSON, instead of trying to parse XML.
+    #
+    if response['Content-Type'] == 'application/json' then
+      if response.body == 'null' or response.body == '{}' then
+        return {}
+      else
+        return JSON.parse(response.body)
+      end
     end
 
+    return true
   end
   
   private :rest_action
