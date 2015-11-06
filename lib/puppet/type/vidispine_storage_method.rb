a0c2ba07d94d4e47ea1b7df4055c1ba93ea4765c
Doug Neal <doug@neal.me.uk>
2015-11-06 11:22:15 +0000

    Fix validation of integer params in vidispine_storage_method type

--- lib/puppet/type/vidispine_storage_method.rb
+++ lib/puppet/type/vidispine_storage_method.rb
@@ -43,9 +43,15 @@ Puppet::Type.newtype(:vidispine_storage_method) do
     desc "The port of the Vidispine API."
 
     validate do |value|
-      unless value =~ /^\d+$/
+      begin
+        Integer(value)
+      rescue ArgumentError
         raise ArgumentError, "%s is not a valid port number." % value
       end
+    end
+
+    munge do |value|
+      Integer(value)
     end
   end
 
