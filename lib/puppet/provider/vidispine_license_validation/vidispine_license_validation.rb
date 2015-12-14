bcfa0d1963332f9fb18529085a7b032749552677
Doug Neal <doug@neal.me.uk>
2015-12-14 13:49:05 +0000

    Rewrite the vidispine_license_validation provider as an exec-style resource rather than an ensurable resource

--- lib/puppet/provider/vidispine_license_validation/vidispine_license_validation.rb
+++ lib/puppet/provider/vidispine_license_validation/vidispine_license_validation.rb
@@ -15,36 +15,5 @@ Puppet::Type.type(:vidispine_license_validation).provide(:vidispine_license_vali
     return nil
   end
 
-  def exists?
-    # Is the license valid already? Check and return true/false straight away
-
-    if license_status == 'valid' then
-      return true
-    else
-      return false
-    end
-  end
-
-  def create
-    # Wait for the system to finish validating.
-    # Keep polling the status until it changes to 'valid' or the timeout expires
-
-    @resource[:timeout].times do
-      if license_status == 'valid' then
-        return true
-      else
-        sleep 1
-      end
-    end
-
-    #return false
-    raise Puppet::Error, 'Timed out waiting for the license to validate.'
-  end
-
-  # We don't expect the destroy method to ever be used, although it may be required
-  def destroy
-    return true
-  end
-
 end
 
--- spec/unit/puppet/provider/vidispine_license_validation/vidispine_license_validation_spec.rb
+++ spec/unit/puppet/provider/vidispine_license_validation/vidispine_license_validation_spec.rb
@@ -17,32 +17,19 @@ describe provider_class do
 
   let(:provider) { resource.provider }
 
-  describe 'exists?' do
-   it 'should return true when the license is valid' do
-     VCR.use_cassette('vidispine_license_validation-exists-true') do
-       expect(provider.exists?).to be_truthy
-     end
-   end
+  describe 'license_status' do
+    it 'should return "valid" when the license is valid' do
+      VCR.use_cassette('vidispine_license_validation-is-valid') do
+        expect(provider.license_status).to eq('valid')
+      end
+    end
 
-   it 'should return false when the license is not valid' do
-     VCR.use_cassette('vidispine_license_validation-exists-false') do
-       expect(provider.exists?).to be_falsy
-     end
-   end
+    it 'should not return "valid" unless the license is valid' do
+      VCR.use_cassette('vidispine_license_validation-is-not-valid') do
+        expect(provider.license_status).not_to eq('valid')
+      end
+    end
   end
 
-  describe 'create' do
-    it 'should fail the Puppet run if the license validation times out' do
-       VCR.use_cassette('vidispine_license_validation-create-timeout') do
-         expect { provider.create }.to raise_error(Puppet::Error, /Timed out waiting for the license to validate/)
-       end
-     end
-
-     it 'should return true when the license validates' do
-       VCR.use_cassette('vidispine_license_validation-create-true') do
-         expect(provider.create).to be_truthy
-       end
-     end
-  end
 end
 
