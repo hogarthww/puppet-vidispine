f5e04514cf3e8697731f029427b7518b6b6c86cd
Doug Neal <doug@neal.me.uk>
2015-12-18 16:31:06 +0000

    Unit tests for vidispine::license::master, vidispine::license::slaveauth

---
+++ spec/unit/defines/license/master_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe 'vidispine::license::master', :type => :define do
+
+  let(:title) { 'master' }
+
+  let(:facts) do
+    {
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistcodename => 'trusty',
+    }
+  end
+
+  let(:pre_condition) do
+    <<-EOT
+    class { '::vidispine':
+      vidispine_version        => '4.2.99',
+      glassfish_das_host       => 'localhost',
+      glassfish_http_port      => '8080',
+      vidispine_admin_user     => 'admin',
+      vidispine_admin_password => 'admin',
+    }
+    EOT
+  end
+
+  describe 'should insist on some content' do
+    it { should raise_error(/content of license can't be empty/) }
+  end
+
+  describe 'should install and validate with default parameters' do
+    let(:params) do
+      {
+        :content => 'rspec',
+      }
+    end
+
+    it {
+      should contain_vidispine_license_validation('master').with( {
+        :timeout => 60,
+        :vsurl   => 'http://localhost:8080',
+        :vsuser  => 'admin',
+        :vspass  => 'admin',
+      } )
+
+      should contain_file('/opt/glassfish3/glassfish/domains/vidispine-domain/License.lic') \
+        .with_content(/rspec/)
+    }
+  end
+
+  describe 'should skip validation if asked' do
+    let(:params) do
+      {
+        :content => 'rspec',
+        :validate => false,
+      }
+    end
+
+    it {
+      should_not contain_vidispine_license_validation('master')
+
+      should contain_file('/opt/glassfish3/glassfish/domains/vidispine-domain/License.lic') \
+        .with_content(/rspec/)
+    }
+  end
+
+end
+
---
+++ spec/unit/defines/license/slaveauth_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe 'vidispine::license::slaveauth', :type => :define do
+
+  let(:title) { 'slave' }
+
+  let(:facts) do
+    {
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistcodename => 'trusty',
+    }
+  end
+
+  let(:pre_condition) do
+    <<-EOT
+    class { '::vidispine':
+      vidispine_version        => '4.2.99',
+      glassfish_das_host       => 'localhost',
+      glassfish_http_port      => '8080',
+      vidispine_admin_user     => 'admin',
+      vidispine_admin_password => 'admin',
+    }
+    EOT
+  end
+
+  describe 'should insist on master node addresses' do
+    it { should raise_error(/master_node_address/) }
+  end
+
+  describe 'with single master and default parameters' do
+    let(:params) do
+      {
+        :master_node_address => [ '192.0.2.99' ]
+      }
+    end
+
+    it {
+      should contain_vidispine_license_validation('slave').with( {
+        :timeout => 60,
+        :vsurl   => 'http://localhost:8080',
+        :vsuser  => 'admin',
+        :vspass  => 'admin',
+      } )
+
+      should contain_file('/opt/glassfish3/glassfish/domains/vidispine-domain/slaveAuth.lic') \
+        .with_content(//)
+    }
+  end
+end
+
